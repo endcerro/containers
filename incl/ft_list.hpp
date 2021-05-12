91b327b48b6585213068fa7da31642b2ace7ca8b
Enzo Dal Cerro <gmx1pt@gmail.com>
2021-05-12 16:57:53 +0200

    it's on rails now

--- incl/ft_list.hpp
+++ incl/ft_list.hpp
@@ -6,7 +6,7 @@
 /*   By: edal--ce <edal--ce@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/08 15:38:23 by edal--ce          #+#    #+#             */
-/*   Updated: 2021/05/12 15:44:30 by edal--ce         ###   ########.fr       */
+/*   Updated: 2021/05/12 16:48:05 by edal--ce         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,22 +41,116 @@ namespace ft
 	 	public :
 	 		ft_list() : _s(0), _h(0), _t(0)
 	 		{
-	 			std::cout << "CONSTRUCTOR CALL" << std::endl;
+	 			// std::cout << "CONSTRUCTOR CALL" << std::endl;
 	 		};
 			~ft_list()
 			{
-				std::cout << "DESTRUCTOR CALL" << std::endl;
+				// std::cout << "DESTRUCTOR CALL" << std::endl;
+				Node *curr = _h;
+				Node *next = 0;
+				while (curr != 0)
+				{
+					next = curr->n;
+					delete (curr);
+					curr = next;
+				}
 			};
 
+
 	 		ft_list& operator=(const ft_list &base){	 		};
-				
-			size_t empty() 	{			};
+			void push_back (const T& val)
+			{
+				if (_t == 0)
+				{
+					_t = new Node;
+					_t->d = val;
+					_t->n = 0;
+					_t->p = 0;
+				}
+				else
+				{
+					_t->n = new Node;
+					_t->n->d = val;
+					_t->n->n = 0;
+					_t->n->p = _t;
+					_t = _t->n;
+				}
+				if (_h == 0)
+					_h = _t;
+				_s++;
+			}
+			T front(void) const
+			{
+				return _h->d;
+			}
+			T back(void) const
+			{
+				return _t->d;
+			}
+			void push_front (const T& val)
+			{
+				if (_h == 0)
+				{
+					_h = new Node;
+					_h->d = val;
+					_h->n = 0;
+					_h->p = 0;
+				}
+				else
+				{
+					Node *tmp = new Node; 
+					tmp->d = val;
+					tmp->n = _h;
+					tmp->p = 0;
+					_h = tmp;
+				}
+				if (_t == 0)
+					_t = _h;
+				_s++;
+			}	
+			void pop_front(void)
+			{
+				Node *tmp = _h;
+				if (_h)	
+					_h = _h->n;
+ 				delete tmp;
+				_s--;				
+			}
+			void emplace_front(T t)
+			{
+				push_front(t);
+			}
+			void pop_back(void)
+			{
+				Node *tmp = _t;
+				if (_t)
+				{
+					_t = _t->p;
+					_t->n = 0;
+				}
+				delete tmp;
+				_s--;				
+			}
+			bool empty() const 	
+			{
+				return (_s > 0 ? true : false);
+			};
 			const size_t size() const
 			{	
 				return _s;
 			};
 			size_t max_size() 	{			};
-		
+			void print()
+			{
+				Node *curr = _h;
+				Node *next = 0;
+				while (curr != 0)
+				{
+					next = curr->n;
+					std::cout << curr->d << std::endl;
+					curr = next;
+				}
+			};
 	};
 }
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,42 @@
 /*   By: edal--ce <edal--ce@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/08 15:34:50 by edal--ce          #+#    #+#             */
-/*   Updated: 2021/05/12 15:36:41 by edal--ce         ###   ########.fr       */
+/*   Updated: 2021/05/12 16:48:45 by edal--ce         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_list.hpp"
-
+#include <list>
 int main()
 {
-	ft::ft_list<int> t;
+	ft::ft_list<std::string> t;
+	t.push_back("haha");
+	t.push_back("Sample");
+	t.push_front("I'll be first ty");
+	// t.print();
+	// std::cout << t.size() << std::endl;
+	// std::cout << t.front() << std::endl;
+	// std::cout << t.back() << std::endl;
+	// t.pop_front()
+	std::cout << t.size() << std::endl;	
+	t.print();
+	t.emplace_front("X1");
+	std::cout << t.size() << std::endl;	
+	t.print();
+
+	// std::cout << t.back() << std::endl;
+
+
+
+	// std::list<std::string> s;
+	// s.push_back("haha");
+	// s.push_back("Sample");
+	// s.push_front("I'll be first ty");
+	// // s.print();
+	// std::cout << s.size() << std::endl;
+	// std::cout << s.front() << std::endl;
+	// std::cout << s.back() << std::endl;
+	// s.pop_back();
+	// std::cout << s.back() << std::endl;
 	return 0;
 }
