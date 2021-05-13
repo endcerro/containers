f16fbd7826234af74dbb4203639204e4abc0652b
Enzo Dal Cerro <gmx1pt@gmail.com>
2021-05-13 17:53:43 +0200

    iterators v1

--- incl/ft_list.hpp
+++ incl/ft_list.hpp
@@ -6,7 +6,7 @@
 /*   By: edal--ce <edal--ce@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/08 15:38:23 by edal--ce          #+#    #+#             */
-/*   Updated: 2021/05/12 17:16:17 by edal--ce         ###   ########.fr       */
+/*   Updated: 2021/05/13 17:52:57 by edal--ce         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,50 +18,181 @@
 #include <iostream>
 
 
-
 namespace ft
 {
 	template <class T>
-	
 	class ft_list
 	{
 
 		struct Node
 		{
-			T		d;
+			T		*d;
 			Node	*n;
 			Node	*p;
 		};
 
-	 	private :
+		private :
 			Node *_h;
 			Node *_t;
 			size_t _s;
 
-	 	public :
-	 		ft_list() : _h(0), _t(0),  _s(0)
-	 		{				};
+		public :
+			ft_list() : _h(0), _t(0),  _s(0)
+			{				};
 			~ft_list()
 			{	clear();	};
 
-	 		ft_list& operator=(const ft_list &base)
-	 		{
-	 			clear();
-	 			
-	 		};
+		   //  class const_iterator
+		// {
+		//     public:
+		//         typedef const_iterator self_type;
+		//         typedef T value_type;
+		//         typedef T& reference;
+		//         typedef T* pointer;
+		//         typedef int difference_type;
+		//         typedef std::forward_iterator_tag iterator_category;
+		//         const_iterator(pointer ptr) : ptr_(ptr) { }
+		//         self_type operator++() { self_type i = *this; ptr_++; return i; }
+		//         self_type operator++(int junk) { ptr_++; return *this; }
+		//         const reference operator*() { return *ptr_; }
+		//         const pointer operator->() { return ptr_; }
+		//         bool operator==(const self_type& rhs) { return ptr_ == rhs.ptr_; }
+		//         bool operator!=(const self_type& rhs) { return ptr_ != rhs.ptr_; }
+		//     private:
+		//         pointer ptr_;
+		// };
+			//Maybe think about const
+			class iterator
+			{
+				public:
+					typedef iterator self_type;
+					typedef T value_type;
+					typedef T& reference;
+					typedef T* pointer;
+					// typedef std::forward_iterator_tag iterator_category;
+					typedef int difference_type;
+					// iterator(pointer ptr) : _ptr(ptr) 
+					// { }
+					iterator(Node *ptr) : _ptr(ptr) 
+					{ }
+					self_type operator++(int) //i++
+					{
+						iterator i = *this; 
+						_ptr = _ptr->n;
+						return i;
+					}
+					self_type operator++() //i++
+					{ 
+						_ptr = _ptr->n; 
+						return *this;
+					}
+					reference operator*() 
+					{ 
+						return *(_ptr->d); 
+					}
+					pointer operator->() 
+					{ 
+						return _ptr->d;
+					}
+					bool operator==(const self_type& rhs)
+					{ 
+						return _ptr == rhs._ptr; 
+					}
+					bool operator!=(const self_type& rhs) 
+					{ 
+						return _ptr != rhs._ptr; 
+					}
+				private :
+					Node *_ptr;
+			};
+
+			class reverse_iterator
+			{
+				public:
+					typedef reverse_iterator self_type;
+					typedef T value_type;
+					typedef T& reference;
+					typedef T* pointer;
+					// typedef std::forward_iterator_tag iterator_category;
+					typedef int difference_type;
+					// iterator(pointer ptr) : _ptr(ptr) 
+					// { }
+					reverse_iterator(Node *ptr) : _ptr(ptr) 
+					{ }
+					self_type operator++(int) //i++
+					{
+						self_type i = *this; 
+						_ptr = _ptr->p;
+						return i;
+					}
+					self_type operator++() //i++
+					{ 
+						_ptr = _ptr->p; 
+						return *this;
+					}
+					reference operator*() 
+					{ 
+						return *(_ptr->d); 
+					}
+					pointer operator->() 
+					{ 
+						return _ptr->d;
+					}
+					bool operator==(const self_type& rhs)
+					{ 
+						return _ptr == rhs._ptr; 
+					}
+					bool operator!=(const self_type& rhs) 
+					{ 
+						return _ptr != rhs._ptr; 
+					}
+				private :
+					Node *_ptr;
+			};
+
+
+			iterator begin()
+			{
+				return iterator(_h);
+			}
+
+			iterator end()
+			{
+				return iterator(_t->n);
+			}
+
+			reverse_iterator rbegin()
+			{
+				return reverse_iterator(_t);
+			}
+
+			reverse_iterator rend()
+			{
+				return reverse_iterator(_h->p);
+			}
+
+			ft_list& operator=(const ft_list &base)
+			{
+				clear();
+				//need to copy also;
+			};
 			void push_back (const T& val)
 			{
 				if (_t == 0)
 				{
 					_t = new Node;
-					_t->d = val;
+					
+					//THIS NEEDS TO USE ALLOCATORS IT SEEMS
+					_t->d = new T(val);
+					
+
 					_t->n = 0;
 					_t->p = 0;
 				}
 				else
 				{
 					_t->n = new Node;
-					_t->n->d = val;
+					_t->n->d = new T(val);
 					_t->n->n = 0;
 					_t->n->p = _t;
 					_t = _t->n;
@@ -75,14 +206,14 @@ namespace ft
 				if (_h == 0)
 				{
 					_h = new Node;
-					_h->d = val;
+					_h->d = new T(val);
 					_h->n = 0;
 					_h->p = 0;
 				}
 				else
 				{
 					Node *tmp = new Node; 
-					tmp->d = val;
+					tmp->d = new T(val);
 					tmp->n = _h;
 					tmp->p = 0;
 					_h = tmp;
@@ -105,7 +236,7 @@ namespace ft
 				Node *tmp = _h;
 				if (_h)	
 					_h = _h->n;
- 				delete tmp;
+				delete tmp;
 				_s--;				
 			}
 			void pop_back(void)
@@ -126,6 +257,7 @@ namespace ft
 				while (curr)
 				{
 					next = curr->n;
+					delete curr->d;
 					delete curr;
 					curr = next;
 				}
@@ -149,7 +281,7 @@ namespace ft
 				while (curr != 0)
 				{
 					next = curr->n;
-					std::cout << curr->d << std::endl;
+					std::cout << *(curr->d) << std::endl;
 					curr = next;
 				}
 			};
--- src/main.cpp
+++ src/main.cpp
@@ -6,34 +6,58 @@
 /*   By: edal--ce <edal--ce@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/08 15:34:50 by edal--ce          #+#    #+#             */
-/*   Updated: 2021/05/12 17:11:39 by edal--ce         ###   ########.fr       */
+/*   Updated: 2021/05/13 17:53:26 by edal--ce         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_list.hpp"
 #include <list>
+
+struct test
+{
+	int a;
+	int b;
+};
+
 int main()
 {
-	ft::ft_list<std::string> t;
-	t.push_back("haha");
-	t.push_back("Sample");
-	t.push_front("I'll be first ty");
+	// i++
+	// ++i
+	ft::ft_list<test> t;
+	test a;
+	a.a = 2;
+	a.b = 3;
+	test c;
+	c.a = 4;
+	c.b = 5;
+	t.push_back(a);
+	t.push_back(c);
+	// t.push_front(800);
+	// // t.print();
+	// // std::cout << t.size() << std::endl;
+	// // std::cout << t.front() << std::endl;
+	// // std::cout << t.back() << std::endl;
+	// // t.pop_front()
+	// std::cout << t.size() << std::endl;	
 	// t.print();
-	// std::cout << t.size() << std::endl;
-	// std::cout << t.front() << std::endl;
-	// std::cout << t.back() << std::endl;
-	// t.pop_front()
-	std::cout << t.size() << std::endl;	
-	t.print();
-	// t.emplace_front("X1");
-	std::cout << t.size() << std::endl;	
-	t.print();
-	t.clear();
-	std::cout << t.size() << std::endl;	
+	// // t.emplace_front("X1");
+	// std::cout << t.size() << std::endl;	
+	// t.print();
+	// t.clear();
+	// std::cout << t.size() << std::endl;	
 
 	// std::cout << t.back() << std::endl;
-
-
+	// ft::ft_list<int>::iterator i = t.begin();
+	std::cout << "Forwad iterator" << std::endl;
+	for(ft::ft_list<test>::iterator i = t.begin(); i != t.end(); i++)
+    {
+        std::cout << i->a <<" "<< i->b << "\n";
+    }
+    std::cout << "Reverse iterator" << std::endl;
+    for(ft::ft_list<test>::reverse_iterator j = t.rbegin(); j != t.rend(); j++)
+    {
+        std::cout << j->a <<" "<< j->b << "\n";
+    }
 
 	// std::list<std::string> s;
 	// s.push_back("haha");
