3c579ade730b69aba082aedb0853899db06be047
Enzo Dal Cerro <gmx1pt@gmail.com>
2021-07-24 13:13:33 +0200

    Tester

--- src/main.cpp
+++ src/main.cpp
@@ -5,78 +5,446 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: edal--ce <edal--ce@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/05/08 15:34:50 by edal--ce          #+#    #+#             */
-/*   Updated: 2021/07/23 19:53:41 by edal--ce         ###   ########.fr       */
+/*   Created: 2021/03/04 15:55:26 by hpottier          #+#    #+#             */
+/*   Updated: 2021/07/24 13:03:37 by edal--ce         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
 #include "../incl/list.hpp"
 #include "../incl/vector.hpp"
 #include "../incl/stack.hpp"
 #include "../incl/map.hpp"
 
-#include <map>
-#include <vector>
-#include <stack>
 #include <list>
+#include <vector>
+#include <map>
+#include <stack>
 #include <string>
+
+#include <ctime>
+#include <cstdlib>
+
+#ifndef TFT
+#define NS std
+#else
+#define NS ft
+#endif
+
+std::string randstr(const size_t n)
+{
+	std::string ret;
+	static const char tab[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+	ret.reserve(n);
+	for (size_t i = 0; i < n; ++i)
+		ret += tab[rand() % 61];
+	return ret;
+}
+
+void lst_testeur()
+{
+	NS::list<int> lst;
+
+	lst.push_back(12);
+	lst.push_back(7);
+	lst.push_back(35);
+	lst.push_back(12);
+	lst.push_back(52);
+	lst.push_back(17);
+	lst.push_back(12);
+	lst.push_back(1);
+	lst.push_back(40);
+	lst.push_back(26);
+	lst.push_back(86);
+	lst.push_back(13);
+	lst.push_back(2);
+	lst.push_back(50);
+	lst.push_back(85);
+	lst.push_back(1);
+	lst.push_back(56);
+	lst.push_back(88);
+	lst.push_back(11);
+	lst.push_back(10);
+	lst.pop_back();
+	lst.pop_front();
+	lst.push_front(12);
+	lst.push_back(10);
+
+	NS::list<int>::iterator it;
+	it = lst.begin();
+	std::cout << "print lst: "
+			  << "<->";
+	while (it != lst.end())
+	{
+		std::cout << '|' << *it << "|<->";
+		++it;
+	}
+	std::cout << std::endl;
+
+	lst.sort();
+	it = lst.begin();
+	std::cout << "print lst after sort: "
+			  << "<->";
+	while (it != lst.end())
+	{
+		std::cout << '|' << *it << "|<->";
+		++it;
+	}
+	std::cout << std::endl;
+	lst.clear();
+	for (int i = 0; i < 1000; ++i)
+	{
+		lst.push_back(rand() % 42845);
+		lst.push_front(rand() % 41134);
+	}
+	lst.sort();
+	NS::list<int>::reverse_iterator rit(lst.rbegin());
+	while (rit != lst.rend())
+	{
+		std::cout << *rit << std::endl;
+		++rit;
+	}
+	for (NS::list<int>::const_reverse_iterator crit = lst.rbegin(); crit != lst.rend(); ++crit)
+		std::cout << *crit << std::endl;
+	std::cout << std::endl;
+
+	lst.assign(8, 42);
+
+	std::vector<int> test;
+	test.push_back(98);
+	test.push_back(-45664);
+	test.push_back(643);
+	test.push_back(-8841);
+	test.push_back(0);
+	test.push_back(10);
+	test.push_back(643);
+	test.push_back(10);
+
+	lst.assign(test.begin(), test.end());
+
+	lst.unique();
+
+	NS::list<int> tlst(32, 42);
+	for (NS::list<int>::iterator xit = tlst.begin(); xit != tlst.end(); ++xit)
+		std::cout << *xit << std::endl;
+	NS::list<int> ttlst(test.begin(), test.end());
+	for (NS::list<int>::iterator xit = ttlst.begin(); xit != ttlst.end(); ++xit)
+		std::cout << *xit << std::endl;
+
+	for (int i = 0; i < 500; ++i)
+	{
+		lst.push_back(rand() % 42845);
+		lst.push_front(rand() % 41134);
+	}
+
+	NS::list<int> ylst;
+
+	for (int i = 0; i < 500; ++i)
+	{
+		ylst.push_back(rand() % 42845);
+		ylst.push_front(rand() % 41134);
+	}
+
+	NS::list<int>::iterator itins(lst.begin());
+	for (int i = 0; i < 52; ++i)
+		++itins;
+	lst.insert(itins, test.begin(), test.end());
+	std::cout << *(lst.insert(itins, 48)) << std::endl;
+
+	ylst.swap(lst);
+	lst.resize(lst.size() - 8);
+
+	it = ylst.begin();
+	while (it != ylst.end())
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+
+	it = lst.begin();
+	++it;
+	++it;
+	++it;
+	lst.splice(it, ylst);
+	lst.push_front(42);
+	lst.remove(42);
+
+	rit = lst.rbegin();
+	while (rit != lst.rend())
+	{
+		std::cout << *rit << std::endl;
+		++rit;
+	}
+	std::cout << std::endl;
+
+	NS::list<std::string> lstr;
+	for (int i = 0; i < 5000; ++i)
+	{
+		lstr.push_back(randstr(rand() % 17 + 1));
+		lstr.push_front(randstr(rand() % 17 + 1));
+	}
+	lstr.reverse();
+
+	NS::list<std::string>::iterator itinstr(lstr.begin());
+	for (int i = 0; i < 52; ++i)
+		++itinstr;
+	lstr.insert(itinstr, 42, "Bonjour je suis un str.");
+
+	NS::list<std::string>::const_iterator cit(lstr.begin());
+	NS::list<std::string>::const_iterator cit2(cit);
+
+	std::cout << *cit2 << std::endl;
+
+	while (cit != lstr.end())
+	{
+		std::cout << *cit << std::endl;
+		++cit;
+	}
+	std::cout << std::endl;
+}
+
+void map_testeur()
+{
+	NS::map<std::string, int> m;
+
+	m.insert(NS::make_pair<std::string, int>("D", 42));
+	m.insert(NS::make_pair<std::string, int>("V", -15));
+	m.insert(NS::make_pair<std::string, int>("E", 684));
+	m.insert(NS::make_pair<std::string, int>("A", -43518));
+	m.insert(NS::make_pair<std::string, int>("B", -4215));
+	m.insert(NS::make_pair<std::string, int>("Z", 6871));
+	m.insert(NS::make_pair<std::string, int>("G", 0));
+	m.insert(NS::make_pair<std::string, int>("R", -15));
+	m.insert(NS::make_pair<std::string, int>("T", 68768415));
+	m.insert(NS::make_pair<std::string, int>("U", -144));
+	m.insert(NS::make_pair<std::string, int>("H", -7852));
+
+	NS::map<std::string, int> mbis(m);
+
+	NS::map<std::string, int>::iterator it(m.insert(NS::make_pair<std::string, int>("E", 123)).first);
+	std::cout << (*it).second << std::endl
+			  << std::endl;
+
+	it = m.begin();
+	while (it != m.end())
+	{
+		std::cout << (*it).first << '	' << (*it).second << std::endl;
+		++it;
+	}
+	std::cout << std::endl;
+
+	NS::map<std::string, int>::reverse_iterator rit(m.rbegin());
+	while (rit != m.rend())
+	{
+		std::cout << (*rit).first << '	' << (*rit).second << std::endl;
+		++rit;
+	}
+	for (NS::map<std::string, int>::const_reverse_iterator crit = m.rbegin(); crit != m.rend(); ++crit)
+		std::cout << (*crit).first << '	' << (*crit).second << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "m size == " << m.size() << std::endl;
+
+	mbis.erase(mbis.find("D"), mbis.end());
+	m.erase(m.find("G"));
+	m.erase(m.find("D"), m.find("R"));
+
+	std::cout << (*m.upper_bound("T")).first << std::endl;
+	std::cout << (*m.lower_bound("T")).first << std::endl;
+	std::cout << std::endl;
+
+	m["N"] = 58;
+	m["N"] = 332;
+
+	it = m.end();
+	while (--it != m.begin())
+		std::cout << (*it).first << '	' << (*it).second << std::endl;
+	std::cout << (*it).first << '	' << (*it).second << std::endl;
+
+	it = mbis.end();
+	while (--it != mbis.begin())
+		std::cout << (*it).first << '	' << (*it).second << std::endl;
+	std::cout << (*it).first << '	' << (*it).second << std::endl;
+
+	std::cout << "m size == " << m.size() << std::endl;
+
+	// m.clear();
+	m.erase(m.begin(), m.end());
+	std::cout << "m size == " << m.size() << std::endl
+			  << std::endl;
+
+	for (int i = 0; i < 100; ++i)
+		m.insert(NS::make_pair<std::string, int>(randstr(rand() % 17 + 1), rand() % 1024));
+
+	NS::map<std::string, int>::const_iterator cit(m.begin());
+	while (cit != m.end())
+	{
+		std::cout << (*cit).first << '	' << (*cit).second << std::endl;
+		++cit;
+	}
+	std::cout << std::endl;
+}
+
+void vector_testeur()
+{
+	NS::vector<int> vec;
+
+	vec.push_back(2);
+	vec.push_back(-48);
+	vec.push_back(841);
+	vec.push_back(11654164);
+	vec.push_back(51);
+	vec.push_back(0);
+	vec.push_back(-8768451);
+	vec.pop_back();
+	vec.pop_back();
+	vec.push_back(16543);
+	vec.push_back(54612);
+	vec.push_back(6164);
+	vec.pop_back();
+	vec.push_back(123);
+	vec.push_back(-42);
+	vec.push_back(124834);
+	vec.push_back(31);
+	vec.push_back(32);
+	vec.push_back(4535);
+
+	vec.reserve(72);
+	vec.resize(72, 42);
+
+	std::cout << vec.at(10) << std::endl;
+	std::cout << vec[10] << std::endl;
+	std::cout << vec.front() << std::endl;
+	std::cout << vec.back() << std::endl;
+
+	NS::vector<int>::iterator it(vec.begin());
+	while (it != vec.end())
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	it = vec.begin();
+	it += 6;
+	NS::vector<int>::iterator zit = it - 4;
+	std::cout << it - zit << std::endl;
+	zit = it + 2;
+	std::cout << *zit << *(3 + zit) << std::endl;
+	std::cout << std::endl;
+
+	vec.clear();
+
+	for (int i = 0; i < 5000; ++i)
+		vec.push_back(rand() % 354351);
+	std::cout << "SIZE = " << vec.size() << std::endl;
+	vec.assign(8, 42);
+	std::cout << "SIZE = " << vec.size() << std::endl;
+
+	std::list<int> test;
+	test.push_back(98);
+	test.push_back(-45664);
+	test.push_back(643);
+	test.push_back(-8841);
+	test.push_back(0);
+	test.push_back(10);
+
+	vec.assign(test.begin(), test.end());
+	std::cout << "SIZE = " << vec.size() << std::endl;
+
+	NS::vector<int> tvec(32, 42);
+	for (NS::vector<int>::iterator xit = tvec.begin(); xit != tvec.end(); ++xit)
+		std::cout << *xit << std::endl;
+	NS::vector<int> ttvec(test.begin(), test.end());
+	for (NS::vector<int>::iterator xit = ttvec.begin(); xit != ttvec.end(); ++xit)
+		std::cout << *xit << std::endl;
+
+	for (int i = 0; i < 500; ++i)
+		vec.push_back(rand() % 354351);
+
+	NS::vector<int>::iterator itins(vec.begin());
+	for (int i = 0; i < 52; ++i)
+		++itins;
+	vec.insert(itins, test.begin(), test.end());
+	std::cout << "SIZE = " << vec.size() << std::endl;
+	itins = vec.begin() + 52;
+		std::cout << *(vec.insert(itins, 48)) << std::endl;
+	
+	std::cout << "SIZE = " << vec.size() << std::endl;
+
+	NS::vector<int>::reverse_iterator rit(vec.rbegin());
+	while (rit != vec.rend())
+	{
+		std::cout << *rit << std::endl;
+		++rit;
+	}
+	for (NS::vector<int>::const_reverse_iterator crit = vec.rbegin(); crit != vec.rend(); ++crit)
+		std::cout << *crit << std::endl;
+	std::cout << std::endl;
+
+	NS::vector<std::string> svec;
+
+	for (int i = 0; i < 5000; ++i)
+		svec.push_back(randstr(rand() % 17 + 1));
+
+	NS::vector<std::string>::iterator itinstr(svec.begin());
+	for (int i = 0; i < 52; ++i)
+		++itinstr;
+	svec.insert(itinstr, 42, "Bonjour je suis un str.");
+
+	NS::vector<std::string>::const_iterator sit(svec.begin());
+	while (sit != svec.end())
+	{
+		std::cout << *sit << std::endl;
+		++sit;
+	}
+	std::cout << std::endl;
+}
+
+void stack_testeur()
+{
+	NS::stack<int> st;
+
+	st.push(42);
+	st.push(-4);
+	st.push(0);
+	st.push(42210);
+	st.pop();
+	st.push(-8654);
+	st.push(12);
+	st.push(1);
+	st.pop();
+	st.push(-468445);
+	st.push(5410);
+	st.push(123);
+	st.pop();
+	st.push(545);
+
+	std::cout << st.top() << '	' << st.size() << std::endl;
+
+	st.pop();
+
+	std::cout << st.top() << '	' << st.size() << std::endl;
+
+	NS::stack<int> st2 = st;
+
+	std::cout << (st2 == st) << std::endl;
+	st2.pop();
+	std::cout << (st2 == st) << std::endl;
+	std::cout << (st2 < st) << std::endl;
+
+	while (st.empty() != true)
+		st.pop();
+	std::cout << st.size() << std::endl
+			  << std::endl;
+}
 
 int main()
 {
-	ft::map<int, char> custom;
-	std::map<int, char> base;
-
-	for (int i = 1; i < 10; i++)
-	{
-		custom.insert(ft::make_pair(i, static_cast<char> (i)));
-		base.insert(std::pair<int, char>(i, static_cast<char> (i)));
-	}
-
-	ft::map<int, char>::iterator cit = custom.begin();
-	std::map<int, char>::iterator bit = base.begin();
-
-	while (cit != custom.end())
-	{
-		std::cout << (cit++)->first << "|";
-	}
-	std::cout << std::endl;
-
-	while (bit != base.end())
-	{
-		std::cout << (bit++)->first << "|";
-	}
-	std::cout << std::endl;
-
-	bit = base.begin();
-	cit = custom.begin();
-
-	++cit;++cit;
-	++bit;++bit;
-
-	ft::map<int, char>::reverse_iterator rcit(cit);// = custom.begin();
-	std::map<int, char>::reverse_iterator rbit(bit);// = base.begin();
-
-
-
-
-
-	std::cout << (rcit)->first << std::endl;
-	std::cout << (rbit)->first << std::endl;
-
-	std::cout << (cit2)->first << std::endl;
-	std::cout << (bit2)->first << std::endl;
-
-
-	// std::cout << (rcit.base())->first << std::endl;
-	// if (rcit.base() == custom.end())
-	// {
-	// 	std::cout << "based\n";
-	// }
-	// if (rbit.base() == base.end())
-	// {
-	// 	std::cout << "based\n";
-	// }
-	// std::cout << (rbit.base())->first << std::endl;
-
-	return 0;
+	// srand(clock());
+	// lst_testeur();
+	map_testeur();
+	vector_testeur();
+	stack_testeur();
+	// queue_testeur();
+	// std::cout << std::endl;
 }
--- test.sh
+++ test.sh
@@ -1,35 +1,36 @@
 #!/bin/bash
 
-echo -en "\033[38;5;208m"
 echo "================================================================================"
 echo "============================== FT_CONTAINERS TEST =============================="
 echo "================================================================================"
-echo -e "\033[0m"
 
-#ADD BACK --98 
-g++ -Wall -Wextra -g3 -fsanitize=address hector_main.cpp -o tstd
+clang++ -Wall -Wextra -Werror -std=c++98 -g3 -fsanitize=address src/main.cpp -o ./obj/test_std
 if [[ $? -ne 0 ]]; then
 	echo -e "\033[31mCOMPILATION ERROR\033[0m"
 	exit
 fi
 
-g++ -Wall -Wextra  -g3 -fsanitize=address -D TFT hector_main.cpp -o tft
+clang++ -Wall -Wextra -Werror -std=c++98 -g3 -fsanitize=address -D TFT src/main.cpp -o ./obj/test_ft
 if [[ $? -ne 0 ]]; then
 	echo -e "\033[31mCOMPILATION ERROR\033[0m"
 	exit
 fi
+echo "Testing STD"
+echo "Timings :"
+time ./obj/test_std > ./results/tstd.results
+echo "STD test done"
+echo ""
 
-./tstd > tstd.results
-echo "TSTD DONE"
-./tft > tft.results
-echo "TFT DONE"
+echo "Testing FT"
+echo "Timings :"
+time ./obj/test_ft > ./results/tft.results
+echo "FT test done"
 
-diff tstd.results tft.results > results.diff
+diff ./results/tstd.results ./results/tft.results > ./results/results.diff
 
 if [[ $? -ne 0 ]]; then
 	echo -e "\033[31mERROR\nsee results.diff\033[0m"
 	exit
 else
 	echo -e "\033[32mEverything is working as intended\033[0m"
-	# rm -rf tstd* tft* results.diff
 fi
